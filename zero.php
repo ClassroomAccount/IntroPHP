3195488919d095c05de6b3ba314445d31d43fef6
Jim Pepe <jpepe@bentley.edu>
2022-09-09 13:43:13 -0400

    Initial commit

---
+++ zero.php
@@ -0,0 +1,29 @@
+<?php
+/* This script will define variables, constants, and echo to a web page
+ Notice that echo may have one or more arguments, comma separated.
+ */
+define("school", "Bentley");   //define constant named school
+
+//define some variables
+$x = 5;
+$y = 23;
+$z = $x + $y;
+$b = true;
+$s = "Bentley";
+
+//echo web page to browser
+echo "<!DOCTYPE html>
+<html>
+    
+<head>
+   <meta charset='UTF-8'>
+   <title>Intro PHP</title>
+</head>
+<body>";
+
+echo "<h3>the score is ", $z, "</h3>";
+
+echo school, " wins again";
+
+echo "</body>
+      </html>";
